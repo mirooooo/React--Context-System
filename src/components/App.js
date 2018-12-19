1936666cc1ed0d307b7d99b45e2399f6098553c3
Miro <mr.mladenoff@gmail.com>
2018-12-19 11:54:21 -0800

    Selecting a Language

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,36 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class App extends Component {
-    render() { 
-        return (<div>App</div>  );
-    }
+  state = {
+    language: "english"
+  };
+
+  onLanguageChange = language => {
+    this.setState({ language });
+  };
+
+  render() {
+    return (
+      <div className="ui container">
+        <div>
+          Select a language:
+          <i
+            onClick={() => {
+              this.onLanguageChange("english");
+            }}
+            className="flag us"
+          />
+          <i
+            onClick={() => {
+              this.onLanguageChange("dutch");
+            }}
+            className="flag nl"
+          />
+        </div>
+        {this.state.language}
+      </div>
+    );
+  }
 }
- 
+
 export default App;
