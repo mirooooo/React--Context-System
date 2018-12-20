7b0dd051580594e7409891883830558d128cab46
Miro <mr.mladenoff@gmail.com>
2018-12-20 11:34:11 -0800

    Accessing Date with Consumers

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,12 +2,18 @@ import React, { Component } from "react";
 import LanguageContext from "../contexts/LanguageContext";
 
 class Button extends Component {
-  // this is how we hook up Class component with Context Object
-  // contextType special name
-  static contextType = LanguageContext;
+  renderSubmit(value) {
+    return value === "english" ? "Submit" : "Voorleggen";
+  }
+
   render() {
-    const text = this.context === "english" ? "Submit" : "Voorlegen";
-    return <button className="ui button primary ">{text}</button>;
+    return (
+      <button className="ui button primary ">
+        <LanguageContext.Consumer>
+          {value => this.renderSubmit(value)}
+        </LanguageContext.Consumer>
+      </button>
+    );
   }
 }
 
